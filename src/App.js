1b113022460cc3baaa07045d23cfa040ebc4354f
Jagannath741 <jagannathpatra718@gmail.com>
2025-06-05 15:10:51 +0530

    All Folder Uploded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { VideoHome } from './components/video-home';
+import { AdminLogin } from './components/admin-login';
+import { UserLogin } from './components/user-login';
+import { AdminDashboard } from './components/admin-dashboard';
+import { AdminAddVideo } from './components/admin-add-video';
+import { AdminEditVideo } from './components/admin-edit-video';
+import { AdminDeleteVideo } from './components/admin-delete-video';
+import { UserRegister } from './components/user-register';
+import { UserDashBoard } from './components/user-dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body-background'>
+      <div className='bg-shade'>
+          <h1 className='text-center text-white pt-4'>Technologies Video Library</h1>
+          <BrowserRouter>
+
+              <Routes>
+                  <Route path='/' element={<VideoHome />} />
+                  <Route path='admin-login' element={<AdminLogin />} />
+                  <Route path='user-login' element={<UserLogin />} />
+                  <Route path='user-register' element={<UserRegister />} />
+                  <Route path='user-dash' element={<UserDashBoard />} />
+                  <Route path='admin-dash' element={<AdminDashboard />} />
+                  <Route path='add-video' element={<AdminAddVideo />} />
+                  <Route path='edit-video/:id' element={<AdminEditVideo />} />
+                  <Route path='delete-video/:id' element={<AdminDeleteVideo />} />
+              </Routes>
+          
+          </BrowserRouter>
+      </div>
     </div>
   );
 }
